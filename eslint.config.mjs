c97f6bd98e8c4042b8acf708113ff3661e532838
MentalWombat <e.ochneva@gmail.com>
2025-10-03 00:18:53 +0300

    configured eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+      'import/no-duplicates': 'error',
+      'import/order': [
+        'error',
+        {
+          named: true,
+          alphabetize: { order: 'asc' },
+          'newlines-between': 'always',
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+        }
+      ]
+    }
+  }),
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts']
+  },
+  eslintConfigPrettier
+];
+
+export default eslintConfig;
